2e33646b86e9d8d1f76226edcdf2f5a0a332a90f
Xingtian Terrencio <tianterrencio125@gmail.com>
2024-12-18 21:42:36 +0800

    aabot pa yan

--- DASHBOARD.Designer.cs
+++ DASHBOARD.Designer.cs
@@ -33,13 +33,13 @@
             // 
             // DASHBOARD
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(814, 488);
+            this.ClientSize = new System.Drawing.Size(1085, 601);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "DASHBOARD";
             this.Text = "DASHBOARD";
             this.ResumeLayout(false);
